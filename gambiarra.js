d59b960514651ecf26e5f53fa9e2695ba4732c1e
Thassae Santos <sasuerj@gmail.com>
2014-03-24 17:48:18 -0300

    Alterações no CSS e validação de campos.

--- gambiarra.js
+++ gambiarra.js
@@ -3,18 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$('#form').submit(function(event) {
+$('#formEmentas').submit(function(event) {
     event.preventDefault();
-    if ($("#form input").val() != '') {
+    if ($("#formEmentas input").val() != '' && $.isNumeric($("#formEmentas input").val())) {
         var createProperties = {
-            url: 'http://www.ementario.uerj.br/pesquisa.php?' + $('#form').serialize(),
+            url: 'http://www.ementario.uerj.br/pesquisa.php?' + $('#formEmentas').serialize(),
             active: true
         };
         chrome.tabs.create(createProperties);
     }
     else
     {
-       $('#modalContent').html('Você realmente precisa digitar o número da disciplina!');
+       $('#modalContent').html('<div class="alert alert-danger">Você realmente precisa digitar o número da disciplina corretamente!<br><br>Cheque se o campo não está em branco ou se não possui caracteres alfanuméricos.</div>');
        $('#alertModal').modal('show');
     }
 });
